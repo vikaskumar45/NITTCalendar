ba9631575c2deda520c6fc040293a58e0475013e
vikaskumar45 <205112045@nitt.edu>
2013-10-15 18:25:26 +0530

    _

---
+++ NITT_CALENDAR/src/nitt_calendar/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nitt_calendar;
+
+/**
+ *
+ * @author vikash
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login l=new Login();
+        l.setVisible(true);
+    }
+
+}
